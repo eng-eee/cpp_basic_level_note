e6c47b2e181d3e0ae46958c1e33363ebf12f616a
Mert Kul <mertkull@outlook.com.tr>
2022-08-30 13:29:43 +0300

    Class konusuna kadar gelindi

--- main.cpp
+++ main.cpp
@@ -1,32 +1,53 @@
 ﻿#include <iostream>
+#include <iterator>
+#include <string>
 
+using namespace std;
+
+
+class AppConstructor {
+public:
+	string msg;
+	int counter;
+
+	AppConstructor() {
+		counter = 5;
+		msg = "Demo message";
+		cout << "AppConstructor called by programmer" << endl;
+	}
+	
+	AppConstructor(string message, int cnt) {
+		msg = message;
+		counter = cnt;
+		cout << "AppConstructor called by programmer1" << endl;
+	}
+	
+	AppConstructor(string message) {
+		msg = message;
+		cout << "AppConstructor called by programmer2" << endl;
+	}
+
+	~AppConstructor() {
+		cout << "AppConstructor destroyed by programmer3" << endl;
+	}
+
+	void disp_msg() {
+		for (size_t i = 0; i < counter; i++)
+		{
+			cout << msg << endl;
+		}
+
+	}
+
+};
 
 int main() {
 	
-	std::cout << "hello world\n";
-	char a = 120;
-	char* ptr = &a;
-	auto b = 5;
-	bool flag = false;
+	AppConstructor demo;
 	
-	uint16_t buf{ 4 };
-	int d(4);
-
-	wchar_t abc = 'b';
-	const double pi = 3.14;
-
-	//int paranter{ 1.2 }; // parantez derleme zamanında hatayi verir
-	int cast_val = 10;
-	cast_val = static_cast<int>(5.8);
-
-	std::cout << abc;
-
-	std::cout<< "buf = "<< buf << "\n";
-	std::cout << "d = "<< d << "\n";
-	std::cout << flag << "\n";
-	std::cout << sizeof(b)<<"\n";
-	std::cout << b << "\n";
-	std::cout << *ptr;
+	//AppConstructor demo("merhaba",4);
+	//AppConstructor demo("selam");
+	demo.disp_msg();
 
 	return 0;
 }
