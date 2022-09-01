37ea9f0ee4ab749a4882d544d0ac7c7a8215fee5
Mert Kul <mertkull@outlook.com.tr>
2022-09-01 10:44:32 +0300

    Cmake ve class yapilari bitti

--- main.cpp
+++ main.cpp
@@ -1,53 +1,28 @@
 ﻿#include <iostream>
-#include <iterator>
-#include <string>
+
 
 using namespace std;
 
-
-class AppConstructor {
+class Kedi {
 public:
-	string msg;
-	int counter;
+	int value{};
 
-	AppConstructor() {
-		counter = 5;
-		msg = "Demo message";
-		cout << "AppConstructor called by programmer" << endl;
-	}
-	
-	AppConstructor(string message, int cnt) {
-		msg = message;
-		counter = cnt;
-		cout << "AppConstructor called by programmer1" << endl;
-	}
-	
-	AppConstructor(string message) {
-		msg = message;
-		cout << "AppConstructor called by programmer2" << endl;
-	}
-
-	~AppConstructor() {
-		cout << "AppConstructor destroyed by programmer3" << endl;
-	}
-
-	void disp_msg() {
-		for (size_t i = 0; i < counter; i++)
-		{
-			cout << msg << endl;
-		}
+	Kedi() {
+		cout << "Created kedi class" << endl;
 
 	}
+	~Kedi() {
+		cout << "Deleted kedi class" << endl;
 
+	}
 };
 
 int main() {
-	
-	AppConstructor demo;
-	
-	//AppConstructor demo("merhaba",4);
-	//AppConstructor demo("selam");
-	demo.disp_msg();
+	shared_ptr<Kedi> kedi_ptr = make_shared<Kedi>();
+	shared_ptr<Kedi> ptr = kedi_ptr;
+	getchar();
+	cout << "value " << ptr->value << endl;
+	cout << "memory address " << ptr.get() << endl; // nesnenin gercek bellek adresi
+	cout << "shared memory pointer numberof element " << ptr.use_count() << endl; // kac isaretci
 
-	return 0;
 }
