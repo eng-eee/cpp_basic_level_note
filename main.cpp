647b8bd8690a45c93b8790282216e048041846fa
Mert Kul <mertkull@outlook.com.tr>
2022-09-06 18:18:51 +0300

    shared pointer added

--- main.cpp
+++ main.cpp
@@ -1,28 +1,7 @@
-﻿#include <iostream>
-
-
+﻿#include <iomanip>
+#include <iostream>
 using namespace std;
-
-class Kedi {
-public:
-	int value{};
-
-	Kedi() {
-		cout << "Created kedi class" << endl;
-
-	}
-	~Kedi() {
-		cout << "Deleted kedi class" << endl;
-
-	}
-};
-
-int main() {
-	shared_ptr<Kedi> kedi_ptr = make_shared<Kedi>();
-	shared_ptr<Kedi> ptr = kedi_ptr;
-	getchar();
-	cout << "value " << ptr->value << endl;
-	cout << "memory address " << ptr.get() << endl; // nesnenin gercek bellek adresi
-	cout << "shared memory pointer numberof element " << ptr.use_count() << endl; // kac isaretci
-
+int main()
+{
+	char degisken_7 = 3;
 }
